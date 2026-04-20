c83d32a277d95fa2a8328d3ea201fa127f60007a
nairx <nairxprv@gmail.com>
2026-04-20 17:15:05 +0530

    updated

--- file23.js
+++ file23.js
@@ -1,19 +1,19 @@
-const fetchData = () => {
+const fetchData = (flag) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      //   resolve("Data Recieved!");
-      reject("Some Error");
+      if (flag) resolve("Data Recieved");
+      else reject("Data Error");
     }, 2000);
   });
 };
 async function getData() {
   try {
     console.log("Start");
-    const result = await fetchData();
+    const result = await fetchData(false);
     console.log(result);
     console.log("End");
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 getData();
